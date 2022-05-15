a0c5ca82bd124314f2d8fea4c0af89c4d4425e69
hamdululum <105590005+hamdululum@users.noreply.github.com>
2022-05-15 21:06:31 +0700

    Add files via upload

---
+++ pertemuan2.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <conio.h>
+ 
+using namespace std;
+int main ()
+{
+float angka;
+int a=0;
+cout<<"Masukkan Bilangan : ";
+cin>>angka;
+a=angka;
+if (angka==a)
+{
+cout<<"Bilangan Bulat";
+}
+else
+{
+cout<<"Bilangan Pecahan";
+}
+getch();
+}
+
